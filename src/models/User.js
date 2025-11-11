e8a7e1620ea579ed52c0d254986a3b3102f8c9b5
Julie <julie.albini@edu.univ-fcomte.fr>
2025-11-11 16:36:11 +0100

    [BUGFIX] Suppression des données dans la bdd

--- src/models/User.js
+++ src/models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema(
-  {
+  { 
     username: { 
       type: String, 
       required: [true, 'Le nom d’utilisateur est requis'], 
