a47d730750fd7cb6531dbaa4d689ccdb5531428e
Julie <julie.albini@edu.univ-fcomte.fr>
2025-11-23 15:40:19 +0100

    [TASK] Création du authMiddleware

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { UserModel, IUser } from '../models/User';
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: IUser;
+    }
+  }
+}
+
+export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.header('Authorization');
+  const token = authHeader?.replace('Bearer ', '');
+
+  if (!token) return res.status(401).json({ error: 'No token provided' });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET || '') as { id: string };
+
+    const user = await UserModel.findById(decoded.id);
+    if (!user) return res.status(401).json({ error: 'User not found' });
+
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(401).json({ error: 'Invalid token' });
+  }
+};
